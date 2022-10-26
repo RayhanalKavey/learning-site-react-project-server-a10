9daa384331b26bc96b3aaf84c5f3be56d6c9e61a
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-26 16:11:46 +0600

    Add structure of the fake data with title and require data.

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ port = process.env.PORT || 5005;
 
 const app = express();
 app.use(cors());
+const tutorials = require("./data/tutorials.json");
 
 app.get("/", (req, res) => {
   res.send("Data is coming.");
+});
+app.get("/tutorials", (req, res) => {
+  res.send(tutorials);
 });
 
 app.listen(port, () => {
